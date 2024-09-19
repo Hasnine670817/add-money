0d3c1ab31e6b2aa95a1b95b8eadd78fb99e2f8c6
Hasnine670817 <hasninetorun@gmail.com>
2024-09-19 20:27:44 +0600

    final-added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// added money
 document.getElementById('add-btn').addEventListener('click', function(event) {
     event.preventDefault();
 
@@ -17,4 +17,48 @@ document.getElementById('add-btn').addEventListener('click', function(event) {
     else {
         alert('Incorrect pin..... please enter right pin')
     }
+});
+
+
+// cash out
+document.getElementById('cash-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const typeAmount = document.getElementById('type-amount1').value;
+    const typePin = document.getElementById('type-pin1').value;
+    const nitAmount = document.getElementById('nit-amount').innerText;
+
+    if (typePin === '1234') {
+        const typeAmountNumber = Number(typeAmount);
+        const nitAmountNumber = Number(nitAmount);
+        const totalBalance = nitAmountNumber - typeAmountNumber;
+        console.log(totalBalance);
+
+        document.getElementById('nit-amount').innerText = totalBalance;
+    }
+    else {
+        alert('Incorrect pin..... please enter right pin')
+    }
+});
+
+
+
+
+
+const moneyId = document.getElementById('add-money');
+const cashId = document.getElementById('cash-out');
+
+const formDiv1 = document.getElementById('form-div1');
+const formDiv2 = document.getElementById('form-div2');
+
+cashId.addEventListener('click', function() {
+
+    formDiv1.style.display = 'none'
+    formDiv2.style.display = 'block'
+});
+
+moneyId.addEventListener('click', function() {
+
+    formDiv1.style.display = 'block'
+    formDiv2.style.display = 'none'
 });
